a388247d05ca45dd71b2de30a4d05136bf1744c3
FelipeSnow <felipenbrito18@outlook.com>
2021-09-18 15:38:55 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { MapView } from 'react-native-maps'
+import MapView from 'react-native-maps'
 
 export default class App extends Component {
   render() {
